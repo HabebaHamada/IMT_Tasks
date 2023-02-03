72635d6354ebf3b83fdabbfc3e6d25efcd6e0455
Habeba Hamada <99296931+HabebaHamada@users.noreply.github.com>
2023-02-03 22:16:19 +0200

    Add files via upload

---
+++ 6-TFT_LCD/TFT_program.c
@@ -0,0 +1,19 @@
+/*************************************************************/
+/*************       Author: Habeba Hamada    ****************/
+/*************       LAYER: HAL               ****************/
+/*************       Component: TFT           ****************/
+/*************       Version: 1.00            ****************/
+/*************************************************************/
+
+
+#include "STD_TYPES.h"
+#include "BIT_Manipulation.h"
+
+#include "GPIO_interface.h"
+#include "STK_interface.h"
+#include "SPI_interface.h"
+
+
+#include "TFT_interface.h"
+#include "TFT_config.h"
+#include "TFT_private.h"
